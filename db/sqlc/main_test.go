a730d4a02183041fbc84fe352e819a2cdf065981
Bitol <bitol@mayaprotocol.com>
2022-12-01 09:06:24 -0600

    Add store + unit test

--- db/sqlc/main_test.go
+++ db/sqlc/main_test.go
@@ -15,14 +15,16 @@ const (
 )
 
 var testQueris *Queries
+var testDB *sql.DB
 
 func TestMain(m *testing.M) {
-	conn, err := sql.Open(dbDriver, dbSource)
+	var err error
+	testDB, err = sql.Open(dbDriver, dbSource)
 	if err != nil {
 		log.Fatal("cannot connect to db:", err)
 	}
 
-	testQueris = New(conn)
+	testQueris = New(testDB)
 
 	os.Exit(m.Run())
 }
